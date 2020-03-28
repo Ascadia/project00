5f9e0c2f0939c25ad37d1004065e235723d277d1
Matteo <Yakumat@gmail.com>
2020-03-28 15:14:43 +0100

    moficato componente App in modo da essere implementato come classe

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Button from "./components/button";
 
-
 // !! PER MANTENERE LA DIMENSIONE DI TAILWIND BASSA !!
 // Don't use string concatenation to create class names
 // https://tailwindcss.com/docs/controlling-file-size
 
-function App() {
-  return (
-    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-      <h1>Super cool page</h1>
-      <Button onClick={() => console.log("I was clicked")}>
-        I am a button
-      </Button>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+        <h1>Super cool page</h1>
+        <Button onClick={() => console.log("I was clicked")}>
+          I am a button
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
